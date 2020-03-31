84f7691553785c40c2ad74af1dff344921473eea
silas <silas@Charybdis>
2020-03-31 13:04:21 +0100

    basic rendering of example covid 19 tracker

--- js/covid19engine.js
+++ js/covid19engine.js
@@ -1,29 +1,26 @@
 /**
- * JS to interface with ID archive API.
- * 
- * ?action=getdirs&name=levels/doom2/
+ * JS to interface with covid 19 tracking API(s).
+ * [adapted from my Doomworld API code]
  */
 var covid19engine = {
 	PROXY_LOCATION : '/proxy/proxycv19_a.php',	
+	container : null,
 	init : function(container){
 		let _container = 'covid19_api_a';
 		if(container){
 			_container = container;
 		}
-
-		this.loadCountries(false,_container,_container);
+		
+		this.loadCountries(_container);
 		this.spinner = document.createElement('img');
 		this.spinner.setAttribute('src','/images/spinner.gif');
 	},
 	
-	loadCountries : function(branch,target,container){
-		console.log("loading data");
-		let _url = covid19engine.PROXY_LOCATION;
-//		if(branch){
-//			_url = covid19engine.PROXY_LOCATION + "?action=getdirs&name=" + branch;
-//		};
-		$('#'+target).find('div.spinner_wrapper').append($(covid19engine.spinner).clone());
-		var _target = target;
+	loadCountries : function(container){
+		console.log( container);
+		let _url = this.PROXY_LOCATION;
+		$('#'+container).find('div.spinner_wrapper').append($(covid19engine.spinner).clone());
+		var _container = container;
 		console.log(_url);
 		$.ajax({
             type: "GET",
@@ -32,35 +29,13 @@ var covid19engine = {
             url : _url
         }).done(function(data){
         	console.log(data);
-        	/* check whether the response is a directory list: */
-//        	if(data['content'] && data['content']['dir']){
-        		covid19engine.buildCountryLinks(data,_target,container);
-//        	}
-        	/* if not, assume a file list */
-//        	else{
-//        		covid19engine.loadFiles(branch,_target);
-//        	};
+       		covid19engine.buildCountryLinks(data,_container);
         }).fail(function(a,b,c){
         	console.log(a,b,c);
         });
 	},
-	
-	loadFiles : function(branch,target,loadedBefore){
-		var _url = covid19engine.PROXY_LOCATION + "?action=getfiles&name=" + branch;
-		var _target = target;
-		$.ajax({
-            type: "GET",
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            url : _url
-        }).done(function(data){
-        	if(data['content'] && data['content']['file']){
-        		covid19engine.buildFileLinks(data,_target);
-        	}
-        });
-	},
 
-	buildCountryLinks : function(data,  target,container){
+	buildCountryLinks : function(data, container){
 		var _out = "";
 		var _ul = document.createElement('ul');
 		_ul.setAttribute('class','doombrowser');
@@ -68,89 +43,111 @@ var covid19engine = {
 		console.log(data);
 		for(let a=0;a<data.data.length;a++){
 			let _li = document.createElement('li');
-//			$('#'+target).append(data.data[a].name + "<br />");
 			var _a1 = document.createElement('a');
 			_a1.setAttribute('href',covid19engine.PROXY_LOCATION + "?code=" + data.data[a].code);
-			
+			_a1.setAttribute('data-countrycode',data.data[a].code);
 			var _txt = document.createTextNode(data.data[a].name);
 			_a1.appendChild(_txt);
 			_li.appendChild(_a1);
 			_ul.appendChild(_li);
 		}
 		console.log(_ul);
-		console.log('#'+target + " > div.panel-text")
-		var _target = $('#'+target + " > div.panel-text");
+		console.log('#'+container + " > div.panel-text")
+		var _target = $('#'+container + " > div.panel-text");
 		_target.append(_ul);
+		this.buildCountryClickHandlers(container);
+	},
+	
+	buildCountryClickHandlers : function(container){
+		$('#'+container + " > div.panel-text").find('a').each(function(){
+			$(this).click(function(){
+				console.log($(this).attr('data-countrycode'));
+				covid19engine.loadCountryDetails($(this).attr('data-countrycode'),container);
+				return(false);
+			});
+		});
 	},
 
-	buildFileLinks : function(data, target){
-		var _target = document.getElementById(target);
-		var _out = "";
-		var ftp_root = "ftp://ftp.fu-berlin.de/pc/games/idgames/";
-		var _ul = document.createElement('ul');
-		if(data.content.file){
-			/* as for directories, if only one result, we don't get an array... */
-			let _files = new Array();
-			if(data.content.file[0] === undefined){
-				_files[0] = data.content.file;
-			}
-			else{
-				_files = data.content.file;
-			}
-			for(var a=0;a<_files.length; a++){
-				var _li = document.createElement('li');
-				_li.setAttribute('data-loaded','false');
-				_li.setAttribute('id','tree_'+_files[a].id);
-				
-				var _div = document.createElement('div');
-				_div.setAttribute('class', 'tree-toggler closed');
-				_div.setAttribute('data-loaded','false');
-				_div.setAttribute('data-target','tree_'+_files[a].id);
-				
-				var _span = document.createElement('span');
-				let _desc = '[no description found]';
-				if(_files[a].description){
-					_desc = _files[a].description.replace(/\<br\>/g,'\n');
-				}
-				let _title = _files[a].filename;
-				if(_files[a].title){
-					_title = _files[a].title
-				}
-				var _a1 = document.createElement('a');
-				_a1.setAttribute('title',_desc);
-				_a1.setAttribute('href',ftp_root + _files[a].dir + _files[a].filename);
-				
-				var _txt = document.createTextNode(_title);
-				_a1.appendChild(_txt);
-				_li.appendChild(_div);
-				_li.appendChild(_a1);
-				_ul.appendChild(_li);
-			}
-			_target.appendChild(_ul);
-			/* remove spinner when done building the childs: */
-			$(_target).find('div.spinner_wrapper > img').remove();
-		}
+	loadCountryDetails : function(cc,container){
+		/*
+		 * AJAX to get details:
+		 * */
+		
+		$.ajax({
+            type: "GET",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            url : covid19engine.PROXY_LOCATION + "?code=" + cc
+        }).done(function(data){
+       		//call func to build details panel
+        	//console.log('#'+container + " > div.panel-text");
+        	$('#'+container + " > div.panel-text").empty();
+        	$('#'+container + " > div.panel-text").append(covid19engine.buildCountryDetails(data));
+        	//add handler for return:
+        	$('#cv19_home').click(function(){
+        		$('#'+container + " > div.panel-text").empty();
+        		covid19engine.loadCountries(container);
+        	});
+        	//then apend a link to go back
+        }).fail(function(a,b,c){
+        	console.log(a,b,c);
+        });
 	},
 	
 	/**
-	 * toggle visibility of child <li> elements 
-	 * and also the folder class of the toggler <div>
+	 * return HTML for dountry details
 	 * */
-	toggleBranch : function(togglerId){
-		var _thing = $('#'+togglerId).find('ul').first();
+	buildCountryDetails : function(data){
+		console.log(data);
+		//summary:
+		let _table1 = document.createElement('table');
+		let _thead1 = document.createElement('thead');
+		let _th1 = document.createElement('th');
+		let _td1a = document.createElement('td');
 		
-		if(_thing.hasClass('hidden')){
-			$('#'+togglerId).find('i').first().removeClass('fa-folder').addClass('fa-folder-open');
-			_thing.removeClass('hidden');
-		}
+		//quick and dirty...
+		let _out = "<h3>"+data.data.name+"</h3>";
+		_out += "<h4>Today</h4><ul>";
+		_out += "<li>Deaths: "+data.data.today.deaths+"</li>";
+		_out += "<li>Confirmed: "+data.data.today.confirmed+"</li>"
+		_out += "</ul><h4>Latest data</h4><ul>";
+		_out += "<li>Deaths: "+data.data.latest_data.deaths+"</li>";
+		_out += "<li>Confirmed: "+data.data.latest_data.confirmed+"</li>";
+		_out += "<li>Recovered: "+data.data.latest_data.recovered+"</li>";
+		_out += "<li>Critical: "+data.data.latest_data.critical+"</li>";
+		_out += "<li>Cases per million: "+data.data.latest_data.calculated.cases_per_million_population+"</li>";
+		_out += "</ul><h4>Timeline</h4>";
+		if(data.data.timeline.length){
+			_out += "<table><thead><tr>"
+				+ "<th>Date</th>"
+				+ "<th>Deaths</th>"
+				+ "<th>Confirmed</th>"
+				+ "<th>Active</th>"
+				+ "<th>Recovered</th>"
+				+ "<th>New</th>"
+				+ "<th>New recovered</th>"
+				+ "<th>New deaths</th>"
+				+ "</tr></thead>";
+			for(let a=0;a<data.data.timeline.length;a++){
+				_out += "<tbody><tr><td>"+data.data.timeline[a]['date']+"</td>"
+					+ "<td>"+data.data.timeline[a]['deaths']+"</td>"
+					+ "<td>"+data.data.timeline[a]['confirmed']+"</td>"
+					+ "<td>"+data.data.timeline[a]['active']+"</td>"
+					+ "<td>"+data.data.timeline[a]['recovered']+"</td>"
+					+ "<td>"+data.data.timeline[a]['new_confirmed']+"</td>"
+					+ "<td>"+data.data.timeline[a]['new_recovered']+"</td>"
+					+ "<td>"+data.data.timeline[a]['new_deaths']+"</td></tr>";
+				}
+			}
 		else{
-			$('#'+togglerId).find('i').first().removeClass('fa-folder-open').addClass('fa-folder');
-			_thing.addClass('hidden');
+			_out += "No timeline data found."
 		}
+		_out += "</tbody></table><div id='cv19_home'>[<a>back</a>]</div>";
+		return(_out);
 	},
 	
 	getQuerystring : function(){
 		return(window.location.search);
 	}
 };
-//$(function(){engine.init();})
+
