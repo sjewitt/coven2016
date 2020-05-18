c4b416d396630c130551ea2a2c7e91bed4465337
Silas <silas@thecoven.co.uk>
2020-05-18 09:23:54 +0100

    weird ahit around file_get_contents and auth. TEST against remote load.

--- proxy/includes/lib.php
+++ proxy/includes/lib.php
@@ -36,6 +36,22 @@ class proxy{
         if($val){
             $req = $req . "/" . $val;
         }
-        return(file_get_contents($req));
+        
+        /*
+         * See: https://stackoverflow.com/questions/26148701/file-get-contents-ssl-operation-failed-with-code-1-failed-to-enable-crypto
+         * LOCAL DEV ONLY!!
+         * */
+        $arrContextOptions=array(
+            "ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ),
+        );
+        
+        
+        $response = file_get_contents("https://maps.co.weber.ut.us/arcgis/rest/services/SDE_composite_locator/GeocodeServer/findAddressCandidates?Street=&SingleLine=3042+N+1050+W&outFields=*&outSR=102100&searchExtent=&f=json", false, stream_context_create($arrContextOptions));
+        
+        
+        return(file_get_contents($req, false, stream_context_create($arrContextOptions)));
     }
 }
