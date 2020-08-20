7cf4154888dae355efd06267bc8feb7f2a8ebd56
smeghammer <silas@thecoven.co.uk>
2020-08-20 16:35:52 +0100

    LETS ACTUALLY PUSH IT...

--- js/covid19engine.js
+++ js/covid19engine.js
@@ -38,52 +38,41 @@ var covid19engine = {
 	buildCountryLinks : function(data, container){
 		var _out = "";
 		let _sel = document.createElement('select');
-		var _ul = document.createElement('ul');
-		_ul.setAttribute('class','doombrowser');
 		
 		console.log(data);
 		for(let a=0;a<data.data.length;a++){
 			let _opt = document.createElement('option');
-			let _li = document.createElement('li');
-			var _a1 = document.createElement('a');
-			_a1.setAttribute('href',covid19engine.PROXY_LOCATION + "?code=" + data.data[a].code);
-			_a1.setAttribute('data-countrycode',data.data[a].code);
 			_opt.setAttribute('value',data.data[a].code);
 			_opt.setAttribute('data-countrycode',data.data[a].code);
 			var _txt = document.createTextNode(data.data[a].name);
-			_a1.appendChild(_txt);
 			_opt.appendChild(_txt);
-			_li.appendChild(_a1);
-			_ul.appendChild(_li);
 			_sel.appendChild(_opt);
 		}
-		console.log(_ul);
 		console.log(_sel);
 		console.log('#'+container + " > div.panel-text")
 		var _target = $('#'+container + " > div.panel-text");
-		//_target.append(_ul);
 		_target.append(_sel);
-		$(_target).on('change',function(){
+		$(_target).off('change click').on('change',function(){
 			let _elm = $('#covid19_api select')[0];
 			console.log(_elm);
 			let _selected = _elm[_elm.selectedIndex].value;
-			
 			console.log(_selected);
 			covid19engine.loadCountryDetails(_selected,container);
 		});
 		
-		this.buildCountryClickHandlers(container);
+		//this.buildCountryClickHandlers(container);
 	},
 	
-	buildCountryClickHandlers : function(container){
-		$('#'+container + " > div.panel-text").find('a').each(function(){
-			$(this).click(function(){
-				console.log($(this).attr('data-countrycode'));
-				covid19engine.loadCountryDetails($(this).attr('data-countrycode'),container);
-				return(false);
-			});
-		});
-	},
+//	buildCountryClickHandlers : function(container){
+//		$('#'+container + " > div.panel-text").find('a').each(function(){
+//			$(this).click(function(){
+//				console.log($(this).attr('data-countrycode'));
+//				console.log('click handler called...');
+//				covid19engine.loadCountryDetails($(this).attr('data-countrycode'),container);
+//				return(false);
+//			});
+//		});
+//	},
 
 	loadCountryDetails : function(cc,container){
 		/*
@@ -96,25 +85,48 @@ var covid19engine = {
             dataType: 'json',
             url : covid19engine.PROXY_LOCATION + "?code=" + cc
         }).done(function(data){
-       		//call func to build details panel
-        	//console.log('#'+container + " > div.panel-text");
+        	//call func to build details panel
         	$('#'+container + " > div.panel-text").empty();
-        	//$('#'+container + " > div.panel-text").append(covid19engine.buildCountryDetails(data));
-        	
-        	//D3 hook:
-        	console.log('D3 hook start');
         	//see https://www.tutorialsteacher.com/d3js/create-svg-chart-in-d3js
-        	let d3data = data.data.timeline;
         	
-        	let width = 1200,
-            scaleFactor = 1,
-            barHeight = 20;
-        	//let graph = d3.select("body").append('svg')
-        	let graph = d3.select('#'+container + " > div.panel-text").append('svg')
-            .attr("width", width)
-            .attr("height", barHeight * d3data.length);
-
+        	let d3data = data.data.timeline;
         	console.log(d3data.length);
+        	let width = 1200;
+        	let barHeight = 20;
+        	let graph = d3.select('#'+container + " > div.panel-text")
+        		.append('svg')
+        		.attr("width", width)
+        		.attr("height", barHeight * d3data.length);
+        	
+        	//in preparation for stacking:
+        	let _fields = ['recovered'];
+        	
+        	//make a STACK object: https://github.com/d3/d3-shape/blob/v1.3.7/README.md#_stack
+        	let _stack = d3.stack();
+        	//set headings:
+        	_stack.keys(_fields);
+        	let _series = _stack(d3data);
+        	
+        	console.log(_series);
+        	//let _x = d3.scaleBand
+        	
+        	
+        	let scaleFactor = 1;
+            
+        	let maxCombinedValue = 0;
+        	let _currentCombinedValue = 0;
+        	for(let a=0;a<d3data.length; a++){
+        		for(let b=0; b<_fields.length; b++){
+        			_currentCombinedValue = d3data[a][_fields[b]];
+        		}
+        		if(maxCombinedValue < _currentCombinedValue){
+        			maxCombinedValue = _currentCombinedValue;
+        		}
+            }
+            //work out the scalefactor:
+            // - max value from data. This needs to be the max for selected field(s). IE max(f1+f2) 'cos we want a ROW
+            scaleFactor = width / maxCombinedValue;
+
         	 let bar = graph.selectAll("g")
              .data(d3data)
              .enter()
@@ -122,11 +134,10 @@ var covid19engine = {
              .attr("transform", function(d, i) {
                    return "translate(0," + i * barHeight + ")";
              });
-        	
         	 
         	 bar.append("rect")
              .attr("width", function(d) {
-            	 console.log(d['recovered']);
+            	 //console.log(d['recovered']);
                  return d['recovered'] * scaleFactor;
 		        })
 		        .attr("height", barHeight - 1);
@@ -136,8 +147,6 @@ var covid19engine = {
 		        .attr("y", barHeight / 2)
 		        .attr("dy", ".35em")
 		        .text(function(d) { return d['recovered']; });
-		     
-		     
 		     
         	let _return = document.createElement('div');
         	_return.setAttribute('id','cv19_home');
@@ -159,13 +168,11 @@ var covid19engine = {
 	 * return HTML for dountry details
 	 * 
 	 * 17052020: Add D3 hooks:
-	 *  - 
+	 *  - move above code to here
 	 * 
 	 * */
 	buildCountryDetails : function(data){
 		console.log(data);
-		
-		
 		
 		//summary:
 		let _table1 = document.createElement('table');
